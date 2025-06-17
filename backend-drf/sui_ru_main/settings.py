e6874a134213773a765ced6da2c06be80b110f47
NyuydineBill <billleynyuy@gmail.com>
2025-06-17 09:36:10 +0100

    added requirements

--- backend-drf/sui_ru_main/settings.py
+++ backend-drf/sui_ru_main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-_(qg#wizm3_*l)xdoo-@7_j$dk!!t5wo=-a7v0&d^$^ovahag@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
