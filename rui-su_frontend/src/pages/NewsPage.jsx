49bfb97378a21a68f4dec9f9bd524e8c8c9b4760
muyah <muyahgaious97@gmail.com>
2025-06-23 10:39:03 +0100

    eslint error fix test

--- rui-su_frontend/src/pages/NewsPage.jsx
+++ rui-su_frontend/src/pages/NewsPage.jsx
@@ -52,7 +52,7 @@ const NewsPage = () => {
   // Load category counts on initial load
   useEffect(() => {
     loadCategoryCounts();
-  }, []);
+  }, [loadCategoryCounts]);
 
   const loadCategoryCounts = async () => {
     const counts = {};
@@ -171,15 +171,6 @@ const NewsPage = () => {
     setAllArticles([]);
     loadInitialData();
     loadCategoryCounts();
-  };
-
-  const formatViews = (num) => {
-    if (num >= 1000000) {
-      return (num / 1000000).toFixed(1) + 'M';
-    } else if (num >= 1000) {
-      return (num / 1000).toFixed(1) + 'K';
-    }
-    return num.toString();
   };
 
   if (loading) {
